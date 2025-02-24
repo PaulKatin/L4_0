b4a9cecfdae6cab0fddec1a5c402a459dd02ef6b
Paul Katin <ka72tin@gmail.com>
2025-02-24 13:17:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using L4_0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        TextMenu textMenu = new TextMenu();
+        Console.ReadLine();
+
+    }
+}
