d350d8909b2ad683293f62f00858b05f1f9fe48b
Paul Katin <ka72tin@gmail.com>
2025-02-24 17:36:04 +0200

    git log --stat New commit. Add new class Employee

--- TextMenu.cs
+++ TextMenu.cs
@@ -6,57 +6,45 @@ using System.Threading.Tasks;
 
 namespace L4_0
 {
-    
-        internal class TextMenu
-        {
-            public void DisplayTextMenu()
-            {
-                while (true)
-                {
-                    Console.WriteLine("Take Choice");
-                    Console.WriteLine("1. Choiсe");
-                    Console.WriteLine("2. Choiсe");
-                    Console.WriteLine("3. Choiсe");
-                    Console.WriteLine("4. Exit");
-                    Console.WriteLine("Select an option: ");
-                    string option = Console.ReadLine();
 
-                    switch (option)
-                    {
-                        case "1":
-                            Choiсe1();
-                            break;
-                        case "2":
-                            Choiсe2();
-                            break;
-                        case "3":
-                            Choiсe3();
-                            break;
-                        case "4":
-                            return;
-                        default:
-                            Console.WriteLine("Invalid option. Please try again.");
-                            break;
-                    }
+    internal class TextMenu
+    {
+        public void DisplayTextMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Take Choice");
+                Console.WriteLine("1. Make Employee 1");
+                Console.WriteLine("2. Make Employee 2");
+                Console.WriteLine("3. Choiсe");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Select an option: ");
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Employee employee1 = new Employee(12, "TestN1", "TestP1", 22);
+                        employee1.DisplayEmployeeInfo();
+                        break;
+                    case "2":
+                        Employee employee2 = new Employee(12, "TestN2", "TestP2", 22);
+                        employee2.DisplayEmployeeInfo();
+                        break;
+                    case "3":
+                        ;
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
                 }
             }
-
-            static void Choiсe1()
-            {
-                Console.WriteLine("Choiсe1");
-
-            }
-
-            static void Choiсe2()
-            {
-                Console.WriteLine("Choiсe2");
-            }
-
-            static void Choiсe3()
-            {
-                Console.WriteLine("Choiсe3");
-            }
         }
+    }
+
 }
+
 
 
